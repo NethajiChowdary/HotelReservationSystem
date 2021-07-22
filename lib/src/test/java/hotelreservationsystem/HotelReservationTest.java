30d566175f19bf1dc6de1e71cd96e35c3a37a3d9
NethajiChowdary <nethajichowdary555@gmail.com>
2021-07-22 22:09:26 +0530

    Added: Code To Check The Cheapest Hotel For a Given Date Range

--- lib/src/test/java/hotelreservationsystem/HotelReservationTest.java
+++ lib/src/test/java/hotelreservationsystem/HotelReservationTest.java
@@ -1,8 +1,10 @@
 package hotelreservationsystem;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 import org.junit.Test;
 
@@ -12,9 +14,9 @@ public class HotelReservationTest
 	@Test
 	public void whenHotelAddedToSystemShouldGetAdded()
 	{
-		Hotel hotel1 = new Hotel("Lakeewood", 110);
-		Hotel hotel2 = new Hotel("Bridgewood", 160);
-		Hotel hotel3 = new Hotel("Ridgewood", 220);
+		Hotel hotel1 = new Hotel("Lakewood", 110, 90, 80, 80, 3);
+		Hotel hotel2 = new Hotel("Bridgewood", 160, 60, 110, 50, 4);
+		Hotel hotel3 = new Hotel("Ridgewood", 220, 150, 100, 40, 5);
 		Hotel[] hotelList = { hotel1, hotel2, hotel3 };
 		List<Hotel> hotels = Arrays.asList(hotelList);
 		HotelReservation hotelReservation = new HotelReservation();
@@ -22,6 +24,21 @@ public class HotelReservationTest
 		hotelReservation.add(hotel2);
 		hotelReservation.add(hotel3);
 		List<Hotel> result = hotelReservation.getHotelList();
-		assertEquals(hotels.size(), result.size());
+		assertEquals(hotels, result);
+	}
+
+	@Test
+	public void whenGivenDateRangeShouldReturnCheapestHotel() 
+	{
+		Hotel hotel1 = new Hotel("Lakewood", 110, 90, 80, 80, 3);
+		Hotel hotel2 = new Hotel("Bridgewood", 160, 60, 110, 50, 4);
+		Hotel hotel3 = new Hotel("Ridgewood", 220, 150, 100, 40, 5);
+		HotelReservation hotelReservation = new HotelReservation();
+		hotelReservation.add(hotel1);
+		hotelReservation.add(hotel2);
+		hotelReservation.add(hotel3);
+		Map<Integer, Hotel> result = hotelReservation.searchFor("10Sep2020", "11Sep2020");
+		result.forEach((k, v) -> System.out.println(v.getName() + " " + k));
+		assertNotNull(result);
 	}
 }
