7876b1015d91478565510eaa6e19415931c85aff
NethajiChowdary <nethajichowdary555@gmail.com>
2021-07-22 20:42:25 +0530

    Added: Code to Add Hotel in a Hotel Reservation System with Name and Rates

--- lib/src/test/java/hotelreservationsystem/HotelReservationTest.java
+++ lib/src/test/java/hotelreservationsystem/HotelReservationTest.java
@@ -1,5 +1,27 @@
 package hotelreservationsystem;
+import static org.junit.Assert.assertEquals;
 
-public class HotelReservationTest {
+import java.util.Arrays;
+import java.util.List;
 
+import org.junit.Test;
+
+public class HotelReservationTest 
+{
+
+	@Test
+	public void whenHotelAddedToSystemShouldGetAdded()
+	{
+		Hotel hotel1 = new Hotel("Lakeewood", 110);
+		Hotel hotel2 = new Hotel("Bridgewood", 160);
+		Hotel hotel3 = new Hotel("Ridgewood", 220);
+		Hotel[] hotelList = { hotel1, hotel2, hotel3 };
+		List<Hotel> hotels = Arrays.asList(hotelList);
+		HotelReservation hotelReservation = new HotelReservation();
+		hotelReservation.add(hotel1);
+		hotelReservation.add(hotel2);
+		hotelReservation.add(hotel3);
+		List<Hotel> result = hotelReservation.getHotelList();
+		assertEquals(hotels.size(), result.size());
+	}
 }
